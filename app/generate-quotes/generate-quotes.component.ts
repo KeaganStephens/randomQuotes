1caa2bc08e581959a695313877a24ff09b874418
Stephens <Keagan.stephens@icloud.com>
2023-10-19 07:34:04 +0200

    basic styling

--- app/generate-quotes/generate-quotes.component.ts
+++ app/generate-quotes/generate-quotes.component.ts
@@ -11,10 +11,14 @@ export class GenerateQuotesComponent {
 
   constructor(private http: HttpClient) {}
 
-  generateQuote() {
-    const category = 'happiness'; // Specify the category you want
-    const apiKey = environment.apiKey; // Replace with your API Key
+  displayQuote = ''
 
+  word_list = ['age', 'alone', 'amazing', 'anger', 'architecture', 'art', 'attitude', 'beauty', 'best', 'birthday', 'business', 'car', 'change', 'communications', 'computers', 'cool', 'courage', 'dad', 'dating', 'death', 'design', 'dreams', 'education', 'environmental', 'equality', 'experience', 'failure', 'faith', 'family', 'famous', 'fear', 'fitness', 'food', 'forgiveness', 'freedom', 'friendship', 'funny', 'future', 'god', 'good', 'government', 'graduation', 'great', 'happiness', 'health', 'history', 'home', 'hope', 'humor', 'imagination', 'inspirational', 'intelligence', 'jealousy', 'knowledge', 'leadership', 'learning', 'legal', 'life', 'love', 'marriage', 'medical', 'men', 'mom', 'money', 'morning', 'movies', 'success']
+  listLength = this.word_list.length
+
+  generateQuote() {
+    const category = this.word_list[Math.floor(Math.random() * this.listLength)]; 
+    const apiKey = environment.apiKey; 
     const headers = new HttpHeaders({
       'X-Api-Key': apiKey
     });
@@ -22,7 +26,7 @@ export class GenerateQuotesComponent {
     this.http.get<any[]>('https://api.api-ninjas.com/v1/quotes?category=' + category, { headers }).subscribe(
       (response) => {
         const randomQuote = response[0]; // Assuming you want only one quote
-        console.log(`"${randomQuote.quote}" - ${randomQuote.author}`);
+        this.displayQuote = `"${randomQuote.quote}" - ${randomQuote.author}`;
       },
       (error) => {
         console.error('Error fetching quotes: ', error);
